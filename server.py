1e069c537b72ba21d5f74af8984ab7ee1f6439b0
Louise P. Myhre <louise-pm@hotmail.no>
2021-05-04 13:51:49 +0200

    started making the routes

--- server.py
+++ server.py
@@ -4,6 +4,25 @@ from flask_restful import Api #vet ikke om man trenger dette
 app = Flask(__name__)
 api = Api(app)
 
+@app.route('/', methods=['GET']) 
+def allItems():
+    pass
+
+@app.route('/api/item/<int:item_id>', methods=['GET']) 
+def oneItem():
+    pass
+
+@app.route('/api/basket', methods=['GET']) #antall varer
+def itemsInCart():
+    pass
+
+@app.route('/api/checkout', methods=['GET']) #alle varer
+def allItemsInBasket():
+    pass
+
+@app.route('/api/order_complete', methods=['GET']) 
+def orderComplete():
+    pass
 
 
 if __name__ == "__main__":
