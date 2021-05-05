426a88e0587cc2e2cab78634b329b3e1f4a53409
Thea <thealp@altiboxmail.no>
2021-05-05 19:52:48 +0200

    send server.py to louise

--- backend/server.py
+++ backend/server.py
@@ -1,25 +1,26 @@
 from flask import Flask, request
 from flask_restful import Api #vet ikke om man trenger dette
 
-import mysql.connector
+'''import mysql.connector #pip install mysql-connector-python
 
 mysql_user = 'user'
 mysql_pwd  = 'G1f3HiAq45'
-mysql_host = 'mysql1'
+mysql_host = 'localhost' #'mysql1'
 mysql_db   = 'shopDB'
 
-mydb = mysql.connector.connect(user = mysql_user, password = mysql_pwd, host = mysql_host, database = mysql_db)
+mydb = mysql.connector.connect(user = mysql_user, password = mysql_pwd, host = mysql_host, port='3306', database = mysql_db)
 
 mycursor = mydb.cursor()
 mycursor.execute("SELECT * FROM items")
-myresult = mycursor.fetchall()
+myresult = mycursor.fetchall()'''
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="./frontend", static_url_path="")
 api = Api(app)
 
-@app.route('/', methods=['GET']) 
-def allItems():
-    return flask.jsonify(myresult)
+@app.route('/', defaults={'path': 'frontend.html'}) 
+'''def allItems():
+    #return flask.jsonify(myresult)
+    pass'''
 
 @app.route('/api/item/<int:item_id>', methods=['GET']) 
 def oneItem():
@@ -38,4 +39,4 @@ def orderComplete():
     pass
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run()
